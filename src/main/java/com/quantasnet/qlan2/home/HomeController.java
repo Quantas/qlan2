0c6456fb942a6cc78458c4c97be8f1196b7c19da
Andrew Landsverk <dewdew@gmail.com>
2015-04-09 19:49:39 -0500

    ssl sanity check

--- src/main/java/com/quantasnet/qlan2/home/HomeController.java
+++ src/main/java/com/quantasnet/qlan2/home/HomeController.java
@@ -1,7 +1,11 @@
 package com.quantasnet.qlan2.home;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by andrewlandsverk on 4/8/15.
@@ -9,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        LOG.info("Secure=", request.isSecure());
         return "home";
     }
 }
