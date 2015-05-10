8efe94e3fd47ef531ee84177539ee8b7d354e5f1
Andrew Landsverk <dewdew@gmail.com>
2015-05-10 10:19:14 -0500

    redirect home controller to org controller

--- src/main/java/com/quantasnet/qlan2/home/HomeController.java
+++ src/main/java/com/quantasnet/qlan2/home/HomeController.java
@@ -3,14 +3,11 @@ package com.quantasnet.qlan2.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by andrewlandsverk on 4/8/15.
- */
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "home";
+        return "redirect:/org";
     }
 }
